dc612b1dfcb0a90b553344eb7cdde011c1a3cc3e
Adi <adib09132005@gmail.com>
2022-08-17 18:45:36 -0500

    create collision phys

--- src/Bullets.java
+++ src/Bullets.java
@@ -10,38 +10,65 @@ import javax.imageio.ImageIO;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bullets
-{
+public class Bullets {
 	private List<Ammo> ammo;
 
-	public Bullets()
-	{
+	public Bullets() {
+		ammo = new ArrayList<Ammo>();
 	}
 
-	public void add(Ammo al)
-	{
+	public void add(Ammo al) {
+		ammo.add(al);
 	}
 
-	//post - draw each Ammo
-	public void drawEmAll( Graphics window )
-	{
+	// post - draw each Ammo
+	public void drawEmAll(Graphics window) {
+		for (int i = 0; i < ammo.size(); i++) {
+			ammo.get(i).draw(window);
+		}
 	}
 
-	public void moveEmAll()
-	{
+	public void moveEmAll() {
+		if (ammo.size() > 0) {
+			for (int i = 0; i < ammo.size(); i++) {
+				ammo.get(i).move("UP");
+			}
+		}
 	}
 
-	public void cleanEmUp()
-	{
+	public void cleanEmUp() {
+		if (ammo.size() > 0) {
+			for (int i = 0; i < ammo.size(); i++) {
+				if (ammo.get(i).getY() < -1) {
+					ammo.get(i).setPos(-1000, -1000);
+				}
+			}
+		}
 	}
 
-	public List<Ammo> getList()
-	{
-		return null;
+	public List<Ammo> getList() {
+		return ammo;
 	}
 
-	public String toString()
-	{
+	public String toString() {
 		return "";
 	}
+
+	public boolean collision(Alien al) {
+		if (ammo.size() > 0) {
+			for (int i = 0; i < ammo.size(); i++) {
+				if ((ammo.get(i).getY() > al.getY()) &&
+						(ammo.get(i).getY() < al.getY() + ammo.get(i).getHeight()) &&
+						(ammo.get(i).getX() > al.getX()) &&
+						(ammo.get(i).getX() < al.getX() + al.getWidth())) {
+					return true;
+				}
+			}
+
+		}
+
+		return false;
+	}
 }
+
+// collision method
